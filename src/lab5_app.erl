51253b4dbc82b68fa3ded8dbb8932823336247ba
Roman Nikolaev <monobearotaku@gmail.com>
2024-05-05 23:14:31 +0300

    Finally i did it

--- src/lab5_app.erl
+++ src/lab5_app.erl
@@ -13,8 +13,8 @@
 start(_StartType, _StartArgs) ->
     N = 5,
     A = init_random_matrix(N),
-    % print_matrix(A),
-    {Matrix, Identity} = inverse_matrix(A, N),
+    print_matrix(A),
+    {_, Identity} = inverse_matrix(A, N),
     print_matrix(Identity),
     {ok, self()}.
 
@@ -45,35 +45,55 @@ init_identity_matrix(N) ->
         end, lists:seq(1, N))
     end, lists:seq(1, N)).
 
-% inverse_matrix(Matrix, N) ->
-%     lists:map(fun(I) ->
-%         Row = lists:nth(I, Matrix),
-%         Pivot = lists:nth(I, Row)
-%         io:format("~f~n", [Pivot])
-% end, lists:seq(1, N)).
-
-
 inverse_matrix(Matrix, N) ->
     Identity = init_identity_matrix(N),
     lists:foldl(fun(I, {Mat, Id}) -> 
         % print_matrix(Id),
         Pivot = getPivot(Mat, I), 
-        ScaledMat = setRow(Mat, scaleRow(getRow(Mat, I), Pivot), I),
-        ScaledId = setRow(Id, scaleRow(getRow(Id, I), Pivot), I),
+        ScaledMat = setRow(Mat, scaleRow(getRow(Mat, I), 1/Pivot), I),
+        ScaledId = setRow(Id, scaleRow(getRow(Id, I), 1/Pivot), I),
 
-        {ScaledMat, ScaledId}    
+        subtractScaledRowFromMatrix(ScaledMat, ScaledId, I, N)
     end, {Matrix, Identity}, lists:seq(1, N)).
 
 getPivot(Matrix, I) ->
     lists:nth(I, getRow(Matrix, I)).
+
+getItem(Matrix, I, J) ->
+    lists:nth(J, getRow(Matrix, I)).
 
 getRow(Matrix, I) ->
     lists:nth(I, Matrix).
 
 scaleRow(Row, Pivot) -> 
     lists:map(fun(Item) -> 
-        Item / Pivot
+        Item * Pivot
     end, Row).
+
+subtractScaledRowFromMatrix(Mat, Id, I, N) ->
+    lists:foldl(fun(J, {AccMat, AccId}) ->
+        if
+            J == I ->
+                {AccMat, AccId};
+            true ->
+                PivotRow = getRow(AccMat, I),
+                CurrentRowMat = getRow(AccMat, J),
+                Factor = getItem(AccMat, J, I),
+
+
+                SubtractedRowMat = subtractRows(CurrentRowMat, scaleRow(PivotRow, Factor)),
+
+                CurrentIdRow = getRow(AccId, J),
+                SubtractedRowId = subtractRows(CurrentIdRow, scaleRow(getRow(AccId, I), Factor)),
+
+                UpdatedMat = setRow(AccMat, SubtractedRowMat, J),
+                UpdatedId = setRow(AccId, SubtractedRowId, J),
+                {UpdatedMat, UpdatedId}
+        end
+    end, {Mat, Id}, lists:seq(1, N)).
+
+subtractRows(Row1, Row2) ->
+    lists:zipwith(fun(X, Y) -> X - Y end, Row1, Row2).
 
 setRow(Matrix, Row, I) ->
     Before = lists:sublist(Matrix, I - 1),   
